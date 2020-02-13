a75c49b2315e146ada71293fe11f658a5355746c
liyuanlin <liyuanlin@qq.com>
2020-02-13 16:45:24 +0800

    add

---
+++ SanHu.Regulatory.Platform/FastDev.Model/Form/loc_event_report.cs
@@ -0,0 +1,14 @@
+namespace FastDev.Model.Form
+{
+   using System;
+   using System.Collections.Generic;
+public partial class loc_event_report : FastDev.Model.Entity.loc_event_report
+{
+public IList<string> EventLocation
+{
+get;
+set;
+}
+}
+
+}
---
+++ SanHu.Regulatory.Platform/FastDev.Model/Report/loc_event_report.cs
@@ -0,0 +1,64 @@
+namespace FastDev.Model.Report
+{
+  using System;
+ using System.Runtime.CompilerServices;
+public partial class loc_event_report
+{
+public string ID
+{
+get;
+set;
+}
+public string EventName
+{
+get;
+set;
+}
+public string Reporter
+{
+get;
+set;
+}
+public string ReporterType
+{
+get;
+set;
+}
+public string Remarks
+{
+get;
+set;
+}
+public string EventLocationID
+{
+get;
+set;
+}
+public DateTime? CreateDate
+{
+get;
+set;
+}
+public string CreateUserID
+{
+get;
+set;
+}
+public DateTime? ModifyDate
+{
+get;
+set;
+}
+public string ModifyUserID
+{
+get;
+set;
+}
+public string Status
+{
+get;
+set;
+}
+}
+
+}
