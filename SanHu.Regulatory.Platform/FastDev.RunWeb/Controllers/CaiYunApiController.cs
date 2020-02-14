6dba28f44bea70d8c9028ad0dbd862af226e2dcc
余盛全 <1586398876@qq.com>
2020-02-14 17:42:44 +0800

    天气api

---
+++ SanHu.Regulatory.Platform/FastDev.RunWeb/Controllers/CaiYunApiController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FastDev.Service;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace FastDev.RunWeb.Controllers
+{
+    public class CaiYunApiController : Controller
+    {
+        public CaiYunApiController(IConfiguration configuration)
+        {
+            _caiYunApiService = new CaiYunApiService(configuration["CaiYunApiToken"]);
+        }
+        private CaiYunApiService _caiYunApiService;
+
+
+        public IActionResult GetWeather()
+        {
+            return Json(_caiYunApiService.GetWeather());
+        }
+    }
+}
