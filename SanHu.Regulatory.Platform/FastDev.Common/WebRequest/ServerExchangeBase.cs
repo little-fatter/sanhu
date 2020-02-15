3bb6730fee1c34d53ba9fa6495cf2e40a6fbf8e4
liyuanlin <liyuanlin@qq.com>
2020-02-15 09:33:57 +0800

    添加类

---
+++ SanHu.Regulatory.Platform/FastDev.Common/WebRequest/ServerExchangeBase.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+
+namespace FastDev.Common
+{
+    public class ServerExchangeBase
+    {
+        public string GetStringFromServer(string sUrl, string functionName, string paramString)
+        {
+            WebMethod wbp = new WebMethod();
+            wbp.Referer = sUrl;
+            wbp.ReqResult = "";
+            wbp.Encode = Encoding.UTF8;
+            wbp.Method = "POST";
+            wbp.PostString = "";
+            wbp.ContentType = "application/x-www-form-urlencoded";
+            wbp.Accept = "application/javascript, */*;q=0.8";
+            wbp.ReqUrl = sUrl + functionName;
+            wbp.PostString = paramString;
+            string rev = wbp.UpLoadString(true);
+            return rev;
+        }
+        public string GetStringFromServer(string functionName, string paramString)
+        {
+            return GetStringFromServer(ConfigurationManager.AppSettings["remote1"] + "/api/", functionName, paramString);
+        }
+        protected T GetDataFromServer<T>(string functionName, string paramString) where T : class,new()
+        {
+            var revStr = GetStringFromServer(functionName, paramString);
+            if (revStr == string.Empty || revStr == "null")
+            {//超时
+                return null;
+            }
+            else
+            {
+                T lRev = JsonHelper.DeserializeJsonToObject<T>(revStr);
+                return lRev;
+            }
+        }
+    }
+}
