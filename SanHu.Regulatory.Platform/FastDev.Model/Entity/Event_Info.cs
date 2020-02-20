286a4298f0dca11b90e014df621b6c91359a2ba5
余盛全 <1586398876@qq.com>
2020-02-20 11:08:30 +0800

    `

--- SanHu.Regulatory.Platform/FastDev.Model/Entity/Event_Info.cs
+++ SanHu.Regulatory.Platform/FastDev.Model/Entity/Event_Info.cs
@@ -1,395 +1,394 @@
 namespace FastDev.Model.Entity
 {
-    using System;
-    using System.Runtime.CompilerServices;
-    public partial class event_info
-    {
-        public string objId
-        {
-            get;
-            set;
-        }
-        public string gldwOid
-        {
-            get;
-            set;
-        }
-        public string reportType
-        {
-            get;
-            set;
-        }
-        public string evtCode
-        {
-            get;
-            set;
-        }
-        public string evtTypeId
-        {
-            get;
-            set;
-        }
-        public string evtTypeIds
-        {
-            get;
-            set;
-        }
-        public string evtFileId
-        {
-            get;
-            set;
-        }
-        public string posFileId
-        {
-            get;
-            set;
-        }
-        public string x1
-        {
-            get;
-            set;
-        }
-        public string y1
-        {
-            get;
-            set;
-        }
-
-        public string lng
-        {
-            get;
-            set;
-        }
-        public string lat
-        {
-            get;
-            set;
-        }
-        public string address
-        {
-            get;
-            set;
-        }
-        public string remark
-        {
-            get;
-            set;
-        }
-        public string targetId
-        {
-            get;
-            set;
-        }
-        public string targetName
-        {
-            get;
-            set;
-        }
-        public string evtState
-        {
-            get;
-            set;
-        }
-        public string wxUserId
-        {
-            get;
-            set;
-        }
-        public string wxCoordinate
-        {
-            get;
-            set;
-        }
-        public string orderNo
-        {
-            get;
-            set;
-        }
-        public string extField
-        {
-            get;
-            set;
-        }
-        public string createTime
-        {
-            get;
-            set;
-        }
-        public string finishTime
-        {
-            get;
-            set;
-        }
-        public string reportTime
-        {
-            get;
-            set;
-        }
-        public string stateIconCode
-        {
-            get;
-            set;
-        }
-        public string finishUserId
-        {
-            get;
-            set;
-        }
-        public string finishUserName
-        {
-            get;
-            set;
-        }
-        public string finishFileId
-        {
-            get;
-            set;
-        }
-        public string reportPhotoUrl
-        {
-            get;
-            set;
-        }
-        public string reportPhotoDto
-        {
-            get;
-            set;
-        }
-        public string finishPhotoUrl
-        {
-            get;
-            set;
-        }
-        public string finishPhotoDto
-        {
-            get;
-            set;
-        }
-        public string reportPhotoGroupId
-        {
-            get;
-            set;
-        }
-        public string finishPhotoGroupId
-        {
-            get;
-            set;
-        }
-        public string evtStateName
-        {
-            get;
-            set;
-        }
-        public string needNotice
-        {
-            get;
-            set;
-        }
-        public string evtTypeDisplayName
-        {
-            get;
-            set;
-        }
-        public string gldwName
-        {
-            get;
-            set;
-        }
-        public string reporterName
-        {
-            get;
-            set;
-        }
-        public string reportPhotoIdList
-        {
-            get;
-            set;
-        }
-        public string finishPhotoList
-        {
-            get;
-            set;
-        }
-        public string dealerName
-        {
-            get;
-            set;
-        }
-        public string auditStateName
-        {
-            get;
-            set;
-        }
-        public string auditState
-        {
-            get;
-            set;
-        }
-        public string finishLimitTime
-        {
-            get;
-            set;
-        }
-        public string evtNos
-        {
-            get;
-            set;
-        }
-        public string objectId
-        {
-            get;
-            set;
-        }
-        public string responseIds
-        {
-            get;
-            set;
-        }
-        public string responseIdList
-        {
-            get;
-            set;
-        }
-        public string responseId
-        {
-            get;
-            set;
-        }
-        public string responseName
-        {
-            get;
-            set;
-        }
-        public string queryBy
-        {
-            get;
-            set;
-        }
-        public string deleteFlagEk
-        {
-            get;
-            set;
-        }
-        public string responseRefId
-        {
-            get;
-            set;
-        }
-        public string dateRange
-        {
-            get;
-            set;
-        }
-        public string startDay
-        {
-            get;
-            set;
-        }
-        public string endDay
-        {
-            get;
-            set;
-        }
-        public string dateType
-        {
-            get;
-            set;
-        }
-        public string reporterIds
-        {
-            get;
-            set;
-        }
-        public string evtStateList
-        {
-            get;
-            set;
-        }
-        public string evtTypeList
-        {
-            get;
-            set;
-        }
-        public string gldwOidList
-        {
-            get;
-            set;
-        }
-        public string responseRefList
-        {
-            get;
-            set;
-        }
-        public string keyWord
-        {
-            get;
-            set;
-        }
-        public string queryModeMp
-        {
-            get;
-            set;
-        }
-        public string timeoutFlag
-        {
-            get;
-            set;
-        }
-        public string finishLimitStartTime
-        {
-            get;
-            set;
-        }
-        public string finishLimitEndTime
-        {
-            get;
-            set;
-        }
-        public string createUser
-        {
-            get;
-            set;
-        }
-        public string tipId
-        {
-            get;
-            set;
-        }
-        public string evtTypeIconCode
-        {
-            get;
-            set;
-        }
-        public string finishLimitId
-        {
-            get;
-            set;
-        }
-        public string timeLimit
-        {
-            get;
-            set;
-        }
-        public string finishRemark
-        {
-            get;
-            set;
-        }
-        public string limitType
-        {
-            get;
-            set;
-        }
-        public string isDangerReport
-        {
-            get;
-            set;
-        }
-        public string dorgId
-        {
-            get;
-            set;
-        }
-    }
+  using System;
+ using System.Runtime.CompilerServices;
+public partial class event_info
+{
+public string objId
+{
+get;
+set;
+}
+public string gldwOid
+{
+get;
+set;
+}
+public string reportType
+{
+get;
+set;
+}
+public string evtCode
+{
+get;
+set;
+}
+public string evtTypeId
+{
+get;
+set;
+}
+public string evtTypeIds
+{
+get;
+set;
+}
+public string evtFileId
+{
+get;
+set;
+}
+public string posFileId
+{
+get;
+set;
+}
+public string x1
+{
+get;
+set;
+}
+public string y1
+{
+get;
+set;
+}
+public string lng
+{
+get;
+set;
+}
+public string lat
+{
+get;
+set;
+}
+public string address
+{
+get;
+set;
+}
+public string remark
+{
+get;
+set;
+}
+public string targetId
+{
+get;
+set;
+}
+public string targetName
+{
+get;
+set;
+}
+public string evtState
+{
+get;
+set;
+}
+public string wxUserId
+{
+get;
+set;
+}
+public string wxCoordinate
+{
+get;
+set;
+}
+public string orderNo
+{
+get;
+set;
+}
+public string extField
+{
+get;
+set;
+}
+public string createTime
+{
+get;
+set;
+}
+public string finishTime
+{
+get;
+set;
+}
+public string reportTime
+{
+get;
+set;
+}
+public string stateIconCode
+{
+get;
+set;
+}
+public string finishUserId
+{
+get;
+set;
+}
+public string finishUserName
+{
+get;
+set;
+}
+public string finishFileId
+{
+get;
+set;
+}
+public string reportPhotoUrl
+{
+get;
+set;
+}
+public string reportPhotoDto
+{
+get;
+set;
+}
+public string finishPhotoUrl
+{
+get;
+set;
+}
+public string finishPhotoDto
+{
+get;
+set;
+}
+public string reportPhotoGroupId
+{
+get;
+set;
+}
+public string finishPhotoGroupId
+{
+get;
+set;
+}
+public string evtStateName
+{
+get;
+set;
+}
+public string needNotice
+{
+get;
+set;
+}
+public string evtTypeDisplayName
+{
+get;
+set;
+}
+public string gldwName
+{
+get;
+set;
+}
+public string reporterName
+{
+get;
+set;
+}
+public string reportPhotoIdList
+{
+get;
+set;
+}
+public string finishPhotoList
+{
+get;
+set;
+}
+public string dealerName
+{
+get;
+set;
+}
+public string auditStateName
+{
+get;
+set;
+}
+public string auditState
+{
+get;
+set;
+}
+public string finishLimitTime
+{
+get;
+set;
+}
+public string evtNos
+{
+get;
+set;
+}
+public string objectId
+{
+get;
+set;
+}
+public string responseIds
+{
+get;
+set;
+}
+public string responseIdList
+{
+get;
+set;
+}
+public string responseId
+{
+get;
+set;
+}
+public string responseName
+{
+get;
+set;
+}
+public string queryBy
+{
+get;
+set;
+}
+public string deleteFlagEk
+{
+get;
+set;
+}
+public string responseRefId
+{
+get;
+set;
+}
+public string dateRange
+{
+get;
+set;
+}
+public string startDay
+{
+get;
+set;
+}
+public string endDay
+{
+get;
+set;
+}
+public string dateType
+{
+get;
+set;
+}
+public string reporterIds
+{
+get;
+set;
+}
+public string evtStateList
+{
+get;
+set;
+}
+public string evtTypeList
+{
+get;
+set;
+}
+public string gldwOidList
+{
+get;
+set;
+}
+public string responseRefList
+{
+get;
+set;
+}
+public string keyWord
+{
+get;
+set;
+}
+public string queryModeMp
+{
+get;
+set;
+}
+public string timeoutFlag
+{
+get;
+set;
+}
+public string finishLimitStartTime
+{
+get;
+set;
+}
+public string finishLimitEndTime
+{
+get;
+set;
+}
+public string createUser
+{
+get;
+set;
+}
+public string tipId
+{
+get;
+set;
+}
+public string evtTypeIconCode
+{
+get;
+set;
+}
+public string finishLimitId
+{
+get;
+set;
+}
+public string timeLimit
+{
+get;
+set;
+}
+public string finishRemark
+{
+get;
+set;
+}
+public string limitType
+{
+get;
+set;
+}
+public string isDangerReport
+{
+get;
+set;
+}
+public string dorgId
+{
+get;
+set;
+}
+}
 
 }
