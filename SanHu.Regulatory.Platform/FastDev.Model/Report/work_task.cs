ae3565cc4cc379a10cdeb8e8dc8372e1db56efc1
liyuanlin <liyuanlin@qq.com>
2020-02-21 15:34:15 +0800

    添加任务

---
+++ SanHu.Regulatory.Platform/FastDev.Model/Report/work_task.cs
@@ -0,0 +1,79 @@
+namespace FastDev.Model.Report
+{
+  using System;
+ using System.Runtime.CompilerServices;
+public partial class work_task
+{
+public string ID
+{
+get;
+set;
+}
+public string Tasktype
+{
+get;
+set;
+}
+public string EventInfoId
+{
+get;
+set;
+}
+public string MainHandler
+{
+get;
+set;
+}
+public string CoOrganizer
+{
+get;
+set;
+}
+public string WorkAddress
+{
+get;
+set;
+}
+public string TaskContent
+{
+get;
+set;
+}
+public string AssignUsersID
+{
+get;
+set;
+}
+public DateTime? InitiationTime
+{
+get;
+set;
+}
+public DateTime? CreateDate
+{
+get;
+set;
+}
+public string CreateUserID
+{
+get;
+set;
+}
+public DateTime? ModifyDate
+{
+get;
+set;
+}
+public string ModifyUserID
+{
+get;
+set;
+}
+public string Status
+{
+get;
+set;
+}
+}
+
+}
---
+++ SanHu.Regulatory.Platform/FastDev.RunWeb/wwwroot/pages/work_task/list.js
@@ -0,0 +1,181 @@
+define([],
+function() {
+
+    var exports = {
+        type: 'list',
+        options: {
+            list: {
+                columns: [{
+                    name: "Tasktype",
+                    display: "任务类型",
+                    type: "string"
+                },
+                {
+                    name: "EventInfoId",
+                    display: "事件ID",
+                    type: "string"
+                },
+                {
+                    name: "InitiationTime",
+                    display: "发起时间",
+                    type: "string"
+                },
+                {
+                    name: "MainHandler",
+                    display: "主办人员",
+                    type: "string"
+                },
+                {
+                    name: "CoOrganizer",
+                    display: "协办人员",
+                    type: "string"
+                },
+                {
+                    name: "WorkAddress",
+                    display: "工作地址",
+                    type: "string"
+                },
+                {
+                    name: "TaskContent",
+                    display: "任务描述",
+                    type: "string"
+                },
+                {
+                    name: "AssignUsers",
+                    display: "分配用户",
+                    type: "ref"
+                }]
+            },
+            common: {
+                formShowType: "tab",
+                formShowPosition: "top",
+                dialogWidth: "700",
+                dialogHeight: "500",
+                showList: 0,
+                showCalendar: 0,
+                showReport: 0,
+                showKanban: 0,
+                hideToolbar: 0,
+                hideViewSwitch: 0,
+                viewNameList: "",
+                viewNameCalendar: "",
+                viewNameReport: "",
+                viewNameKanban: "",
+                searchInputShowType: "right",
+                buttonsShowType: "",
+                searchBoxShowType: "",
+                searchAdShowType: "",
+                formViewName: "",
+                openParm: ""
+            },
+            type: "list",
+            filterFields: [{
+                display: "分配用户",
+                name: "AssignUsers",
+                editor: {
+                    url: "/web/namedata",
+                    parms: {
+                        model: "user"
+                    },
+                    detailEnabled: true,
+                    detailUrl: "/web/detaildata",
+                    detailParms: {
+                        model: "user"
+                    },
+                    valueField: "ID",
+                    sourceFilter: null,
+                    textField: "Name",
+                    css: "combobox-selector",
+                    popupselect_ismul: true,
+                    popupselect_type: "popupselect",
+                    popupselect_url: "/web/main/?model=user&viewtype=list",
+                    popupselect_width: "1000",
+                    popupselect_height: "700",
+                    popupselect_title: "选择： frameworkuser",
+                    many2one: false,
+                    one2many: false,
+                    many2many: true,
+                    type: "ref_popupselect_mul"
+                },
+                type: "ref_popupselect_mul"
+            },
+            {
+                display: "任务类型",
+                name: "Tasktype",
+                editor: {
+                    type: "string"
+                },
+                type: "string"
+            },
+            {
+                display: "事件ID",
+                name: "EventInfoId",
+                editor: {
+                    type: "string"
+                },
+                type: "string"
+            },
+            {
+                display: "发起时间",
+                name: "InitiationTime",
+                editor: {
+                    type: "string"
+                },
+                type: "string"
+            },
+            {
+                display: "主办人员",
+                name: "MainHandler",
+                editor: {
+                    type: "string"
+                },
+                type: "string"
+            },
+            {
+                display: "协办人员",
+                name: "CoOrganizer",
+                editor: {
+                    type: "string"
+                },
+                type: "string"
+            },
+            {
+                display: "工作地址",
+                name: "WorkAddress",
+                editor: {
+                    type: "string"
+                },
+                type: "string"
+            },
+            {
+                display: "任务描述",
+                name: "TaskContent",
+                editor: {
+                    type: "string"
+                },
+                type: "string"
+            },
+            {
+                display: "AssignUsersID",
+                name: "AssignUsersID",
+                editor: {
+                    type: "text"
+                },
+                type: "text"
+            }],
+            link: {},
+            addins: {}
+        },
+        dataset: 'web/dataset?model=work_task&viewname=list'
+    };
+    exports.options.model = {
+        name: 'work_task',
+        title: '任务'
+    };
+
+    exports.service = function service(page) {
+
+};
+
+    return exports;
+});
