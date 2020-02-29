c161a22b661bac95bfd3e4dd3b7e786de49890d2
余盛全 <1586398876@qq.com>
2020-02-29 16:26:10 +0800

    新增接口获取指定任务的表单数据

--- SanHu.Regulatory.Platform/FastDev.Model/Entity/attachment.cs
+++ SanHu.Regulatory.Platform/FastDev.Model/Entity/attachment.cs
@@ -1,77 +1,77 @@
 namespace FastDev.Model.Entity
 {
-  using System;
- using System.Runtime.CompilerServices;
-public partial class attachment
-{
-public string ID
-{
-get;
-set;
-}
-public string attach_type
-{
-get;
-set;
-}
-public string url
-{
-get;
-set;
-}
-public string thumbnail
-{
-get;
-set;
-}
-public string Remark
-{
-get;
-set;
-}
-public string Associatedobjecttype
-{
-get;
-set;
-}
-public string AssociationobjectID
-{         
-get;
-set;
-}
-public string CorrelationId
-{
-get;
-set;
-}
-public DateTime? CreateDate
-{
-get;
-set;
-}
-public string CreateUserID
-{
-get;
-set;
-}
-public DateTime? ModifyDate
-{
-get;
-set;
-}
-public string ModifyUserID
-{
-get;
-set;
-}
-public string Status
-{
-get;
-set;
-}
-public string fileName
+    using System;
+    using System.Runtime.CompilerServices;
+    public partial class attachment
+    {
+        public string ID
         {
-            get;set;
+            get;
+            set;
+        }
+        public string attach_type
+        {
+            get;
+            set;
+        }
+        public string url
+        {
+            get;
+            set;
+        }
+        public string thumbnail
+        {
+            get;
+            set;
+        }
+        public string Remark
+        {
+            get;
+            set;
+        }
+        public string Associatedobjecttype
+        {
+            get;
+            set;
+        }
+        public string AssociationobjectID
+        {
+            get;
+            set;
+        }
+        public string CorrelationId
+        {
+            get;
+            set;
+        }
+        public DateTime? CreateDate
+        {
+            get;
+            set;
+        }
+        public string CreateUserID
+        {
+            get;
+            set;
+        }
+        public DateTime? ModifyDate
+        {
+            get;
+            set;
+        }
+        public string ModifyUserID
+        {
+            get;
+            set;
+        }
+        public string Status
+        {
+            get;
+            set;
+        }
+        public string fileName
+        {
+            get; set;
         }
 
         public string fileSize
