c5531f4fe50f6c1f8491b5dbd2e2ec890aff6493
liyuanlin <liyuanlin@qq.com>
2020-02-29 14:00:08 +0800

    链接地址

--- SanHu.Regulatory.Platform/FastDev.RunWeb/wwwroot/pages/attachment/list.js
+++ SanHu.Regulatory.Platform/FastDev.RunWeb/wwwroot/pages/attachment/list.js
@@ -16,14 +16,31 @@ function() {
                     type: "string"
                 },
                 {
+                    width: "150",
+                    display: "关联对象的Id",
+                    name: "AssociationobjectID",
+                    name_text: "关联对象的Id",
+                    align: "left",
+                    align_textfield: "左对齐",
+                    type: "string",
+                    type_text: "文本型",
+                    editorType: ""
+                },
+                {
                     name: "thumbnail",
                     display: "缩略图地址",
                     type: "string"
                 },
                 {
-                    name: "CorrelationID",
-                    display: "关联id",
-                    type: "string"
+                    width: "150",
+                    display: "fileCode",
+                    name: "fileCode",
+                    name_text: "fileCode",
+                    align: "left",
+                    align_textfield: "左对齐",
+                    type: "string",
+                    type_text: "文本型",
+                    editorType: ""
                 },
                 {
                     name: "Remark",
@@ -34,8 +51,24 @@ function() {
             common: {
                 formShowType: "tab",
                 formShowPosition: "top",
-                dialogWidth: 700,
-                dialogHeight: 500
+                dialogWidth: "700",
+                dialogHeight: "500",
+                showList: 0,
+                showCalendar: 0,
+                showReport: 0,
+                showKanban: 0,
+                hideToolbar: 0,
+                hideViewSwitch: 0,
+                viewNameList: "",
+                viewNameCalendar: "",
+                viewNameReport: "",
+                viewNameKanban: "",
+                searchInputShowType: "",
+                buttonsShowType: "",
+                searchBoxShowType: "",
+                searchAdShowType: "",
+                formViewName: "",
+                openParm: ""
             },
             type: "list",
             filterFields: [{
@@ -63,16 +96,80 @@ function() {
                 type: "string"
             },
             {
-                display: "关联id",
-                name: "CorrelationID",
+                display: "备注",
+                name: "Remark",
                 editor: {
                     type: "string"
                 },
                 type: "string"
             },
             {
-                display: "备注",
-                name: "Remark",
+                display: "关联表单的类型",
+                name: "Associatedobjecttype",
+                editor: {
+                    type: "string"
+                },
+                type: "string"
+            },
+            {
+                display: "关联对象的Id",
+                name: "AssociationobjectID",
+                editor: {
+                    type: "string"
+                },
+                type: "string"
+            },
+            {
+                display: "关联Id",
+                name: "CorrelationId",
+                editor: {
+                    type: "string"
+                },
+                type: "string"
+            },
+            {
+                display: "空间",
+                name: "spaceId",
+                editor: {
+                    type: "string"
+                },
+                type: "string"
+            },
+            {
+                display: "文件名",
+                name: "fileName",
+                editor: {
+                    type: "string"
+                },
+                type: "string"
+            },
+            {
+                display: "文件大小",
+                name: "fileSize",
+                editor: {
+                    type: "string"
+                },
+                type: "string"
+            },
+            {
+                display: "文件类型",
+                name: "fileType",
+                editor: {
+                    type: "string"
+                },
+                type: "string"
+            },
+            {
+                display: "文件id",
+                name: "fileId",
+                editor: {
+                    type: "string"
+                },
+                type: "string"
+            },
+            {
+                display: "fileCode",
+                name: "fileCode",
                 editor: {
                     type: "string"
                 },
