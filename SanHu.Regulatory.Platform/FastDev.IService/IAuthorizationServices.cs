30b5f17d6d09966cb07cf47d4581bd1ee6c7d383
luoxinliu <luoxinliu@vip.qq.com>
2020-03-03 10:28:03 +0800

    授权兼容新版本

---
+++ SanHu.Regulatory.Platform/FastDev.IService/IAuthorizationServices.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastDev.IServices
+{
+    public interface IAuthorizationServices: IApplicationServices
+    {
+        /// <summary>
+        /// 去授权
+        /// </summary>
+        /// <returns>是否成功</returns>
+        Task<bool> ToAuthor();
+    }
+}
