cea2ef8a4e4667c7dafd2cc9c53aa272f95391d3
罗新柳 <luoxinliu@vip.qq.com>
2020-03-10 16:12:40 +0800

    从框架获取用户信息服务，类库换成netstandard2.1

---
+++ SanHu.Regulatory.Platform/FastDev.Model/Dto/UserDetailsDTO.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace FD.Model.Dto
+{
+    public class UserDetailsDTO 
+    {
+        public List<RoleDetailsDTO> Roles { get; set; }
+        public List<UserOrganizationInfoDTO> Organizations { get; set; }
+    }
+}
